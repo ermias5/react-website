d5e4f0836de894dbf1d81c81824978950f83703b
ermias alehegn <144139764+ermias5@users.noreply.github.com>
2023-12-08 21:28:39 +0300

    Add files via upload

---
+++ App.js
@@ -0,0 +1,25 @@
+import React from "react";
+import { Box } from "@mui/material";
+//import Feed from "./components/Feed";
+import SideBar from "./components/NavBar";
+//import HeaderAppBar from "./components/Header";
+
+
+const App = () => {
+
+
+  return (
+    <Box>
+      {/*<HeaderAppBar />*/}
+
+
+
+      <SideBar />
+      {/*<Feed />*/}
+
+
+    </Box>
+  )
+};
+
+export default App;
